b31395b0a4fde9a2594e72263768e2342f6b3398
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-06-02 12:34:46 -0400

    useEffect with async

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import Header from "./components/Header"
 import Meme from "./components/Meme"
 import './App.css';
 
-
-/**
- * Challenge: Build the Header component
- */
 export default function App() {
     return (
         <div>
